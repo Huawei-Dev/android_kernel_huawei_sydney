99467a328a35b24cd3e17e5e89e83ca39422ddfe
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2017-11-08 12:13:47 -0800

    ANDROID: sched/walt: Fix compilation issue for x86_64
    Below compilation errors observed when SCHED_WALT enabled and
    FAIR_GROUP_SCHED is disabled, fix it.
    
      CC      kernel/sched/walt.o
    kernel/sched/walt.c: In function .walt_inc_cfs_cumulative_runnable_avg.:
    kernel/sched/walt.c:157:8: error: .struct cfs_rq. has no member named .cumulative_runnable_avg.
      cfs_rq->cumulative_runnable_avg += p->ravg.demand;
            ^
    kernel/sched/walt.c: In function .walt_dec_cfs_cumulative_runnable_avg.:
    kernel/sched/walt.c:163:8: error: .struct cfs_rq. has no member named .cumulative_runnable_avg.
      cfs_rq->cumulative_runnable_avg -= p->ravg.demand;
            ^
    make[2]: *** [kernel/sched/walt.o] Error 1
    make[1]: *** [kernel/sched] Error 2
    make: *** [kernel] Error 2
    
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -378,6 +378,7 @@ static int __init walt_init_ops(void)
 }
 late_initcall(walt_init_ops);
 
+#ifdef CONFIG_CFS_BANDWIDTH
 void walt_inc_cfs_cumulative_runnable_avg(struct cfs_rq *cfs_rq,
 		struct task_struct *p)
 {
@@ -387,6 +388,7 @@ void walt_dec_cfs_cumulative_runnable_avg(struct cfs_rq *cfs_rq,
 		struct task_struct *p)
 {
 }
+#endif
 
 static void
 update_window_start(struct rq *rq, u64 wallclock)

2f50d5f6fb208099690f4cdbbd18b36a78f18427
Eric Biggers <ebiggers@google.com>
2018-01-03 11:16:25 -0800

    crypto: poly1305 - remove ->setkey() method
    commit a16e772e664b9a261424107784804cffc8894977 upstream.
    
    Since Poly1305 requires a nonce per invocation, the Linux kernel
    implementations of Poly1305 don't use the crypto API's keying mechanism
    and instead expect the key and nonce as the first 32 bytes of the data.
    But ->setkey() is still defined as a stub returning an error code.  This
    prevents Poly1305 from being used through AF_ALG and will also break it
    completely once we start enforcing that all crypto API users (not just
    AF_ALG) call ->setkey() if present.
    
    Fix it by removing crypto_poly1305_setkey(), leaving ->setkey as NULL.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- crypto/poly1305_generic.c
+++ crypto/poly1305_generic.c
@@ -58,6 +58,11 @@ void poly1305_core_setkey(struct poly1305_key *key, const u8 *raw_key)
 }
 EXPORT_SYMBOL_GPL(poly1305_core_setkey);
 
+/*
+ * Poly1305 requires a unique key for each tag, which implies that we can't set
+ * it on the tfm that gets accessed by multiple users simultaneously. Instead we
+ * expect the key as the first 32 bytes in the update() call.
+ */
 unsigned int crypto_poly1305_setdesckey(struct poly1305_desc_ctx *dctx,
 					const u8 *src, unsigned int srclen)
 {

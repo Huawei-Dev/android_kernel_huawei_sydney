4ba6e804cd8795bd2e0483afa7370e458b3ff1ff
Eric Biggers <ebiggers@google.com>
2018-01-11 23:30:13 -0500

    fscrypt: remove 'ci' parameter from fscrypt_put_encryption_info()
    fscrypt_put_encryption_info() is only called when evicting an inode, so
    the 'struct fscrypt_info *ci' parameter is always NULL, and there cannot
    be races with other threads.  This was cruft left over from the broken
    key revocation code.  Remove the unused parameter and the cmpxchg().
    
    Also remove the #ifdefs around the fscrypt_put_encryption_info() calls,
    since fscrypt_notsupp.h defines a no-op stub for it.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- include/linux/fscrypt_supp.h
+++ include/linux/fscrypt_supp.h
@@ -121,7 +121,7 @@ extern int fscrypt_derive_gcm_key(struct crypto_aead *,
 				u8 *, u8 *, u8 *, int);
 extern struct key *fscrypt_request_key(u8 *, const u8 *, int);
 extern int fscrypt_get_encryption_info(struct inode *);
-extern void fscrypt_put_encryption_info(struct inode *, struct fscrypt_info *);
+extern void fscrypt_put_encryption_info(struct inode *);
 
 /* fname.c */
 extern int fscrypt_setup_filename(struct inode *, const struct qstr *,

f0f6771ecc26852158156c326479824c9dd3c75c
Huawei_Dev <surdu_petru@yahoo.com>
2024-12-22 20:12:05 +0100

    defconfig: drop unused flags CONFIG_HUAWEI_DATA_ACQUISITION/SENSOR_DATA_ACQUISITION

--- include/huawei_platform/power/batt_acr_detect.h
+++ include/huawei_platform/power/batt_acr_detect.h
@@ -11,23 +11,10 @@
 #define ACR_RT_FMD_MIN_DEFAULT 40
 #define ACR_RT_FMD_MAX_DEFAULT 90
 
-#ifdef CONFIG_HUAWEI_DATA_ACQUISITION
-#define ACR_RT_DEFAULT_VERSION	1
-#define ACR_RT_ITEM_ID			703023001
-#define ACR_RT_FMD_FAIL			"fail"
-#define ACR_RT_FMD_PASS			"pass"
-#define ACR_RT_NA				"NA"
-#define ACR_RT_DEVICE_NAME		"battery"
-#define ACR_RT_TEST_NAME		"BATT_ACR_VAL"
-#endif
-
 struct acr_device_info {
 	struct device *dev;
 	struct acr_info acr_data;
 	struct delayed_work acr_rt_work;
-#ifdef CONFIG_HUAWEI_DATA_ACQUISITION
-	struct message acr_msg;
-#endif
 	int acr_rt_support;
 	int acr_rt_status;
 	int acr_rt_threshold;
